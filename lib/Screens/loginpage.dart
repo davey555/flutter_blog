ce82d012efda5f14147447e1afbcc219e45f71ad
davey555 <dalonewolf47@gmail.com>
2020-12-02 00:21:56 +0100

    made changes to the file structure as i started and also had to get assets to start with

---
+++ lib/Screens/loginpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ]
+        ), 
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
---
+++ lib/Screens/screens.dart
@@ -0,0 +1,4 @@
+export 'loginpage.dart';
+export 'bloghomepage.dart';
+export 'blogfeed.dart';
+export 'blogpost.dart';
